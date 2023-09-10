c6874beb16d81ec2a494ad51ebc3fbdccda64cf0
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-09-10 15:52:09 +0200

    Add files via upload

---
+++ prova/loggato.php
@@ -0,0 +1,34 @@
+<?php
+include_once("connect.php");
+	
+$name = trim($_POST["username"]);
+$pass = trim($_POST["password"]);
+$i=0;
+
+if(strlen($name)==0){
+	echo("Inserire il nome utente!");
+	$i=$i+1;
+}
+
+if(strlen($pass)==0){
+	echo("Inserire la password!");
+	$i=$i+1;
+}
+
+if($i==0){
+	$resultset_utente_passw = mysqli_query($link, "SELECT Username, Passw FROM utente WHERE Username='$name'") or die("database error:". mysqli_error($link));
+
+	while($row = mysqli_fetch_assoc($resultset_utente_passw)){
+		if($row['Passw']!= $pass ){
+			echo("Username o Password non validi");
+			$i=$i+1;
+			break;
+		}
+	}
+}
+
+if($i==0){
+	echo "OK";
+}
+
+?>
