6dde594fb548bcc9ab1828763fe6cf589d6661c5
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-09-11 16:37:39 +0200

    Update registrazione.php

--- prova/registrazione.php
+++ prova/registrazione.php
@@ -75,36 +75,52 @@ session_unset();
       </script>
    </head>
    <body>
-      <div class="wrapper">
-         <div class="title">
-            Registrati
+      <div class="grid-container">
+         <div class="testo">
+            <h2>Bluggle</h2>
+            <h3><span>crea,</span></h3>
+            <h3><span>commenta,</span></h3>
+            <h3><span>condividi.</span></h3>
          </div>
-         <form id="registrati" action="registrato.php" method="post">
-            <div class="field">
-               <input type="text" name="mail" id="mail">
-               <label>Indirizzo Email</label>
+         <div class="wrapper">
+            <div class="title">
+               Registrati
             </div>
-            <div class="field">
-               <input type="text" name="username" id="username">
-               <label>Username</label>
+            <form id="registrati" action="registrato.php" method="post">
+               <div class="field">
+                  <input type="text" name="mail" id="mail">
+                  <label>Indirizzo Email</label>
+               </div>
+               <div class="field">
+                  <input type="text" name="username" id="username">
+                  <label>Username</label>
+               </div>
+               <div class="field">
+                  <input type="password" name="password" id="password">
+                  <label>Password</label>
+               </div>
+               <div class="field">
+                  <input type="password" name="password_2" id="password_2">
+                  <label>Conferma Password</label>
+               </div>
+            </br>
+            <p id="error_message"></p>
+               <div class="field">
+                  <input type="submit" value="Salva">
+               </div>
+               <div class="signup-link">
+                  Hai già un account? <a href="login.php">Accedi</a>
+               </div>
+            </form>
+         </div>
+         <div class="images">
+            <div class="img_1">
+               <img src="foto/selfie.jpg" alt="selfie">
             </div>
-            <div class="field">
-               <input type="password" name="password" id="password">
-               <label>Password</label>
+            <div class="img_2">
+               <img src="foto/social_7.jpg" alt="social">
             </div>
-            <div class="field">
-               <input type="password" name="password_2" id="password_2">
-               <label>Conferma Password</label>
-            </div>
-         </br>
-         <p id="error_message"></p>
-            <div class="field">
-               <input type="submit" value="Salva">
-            </div>
-            <div class="signup-link">
-               Hai già un account? <a href="login.php">Accedi</a>
-            </div>
-         </form>
+         </div>
       </div>
    </body>
 </html>
