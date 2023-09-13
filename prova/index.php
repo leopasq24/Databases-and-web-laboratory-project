ada1514ec055e5fa5d5311ec2ff4739ecc2b4881
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-09-13 13:03:50 +0200

    Update index.php

--- prova/index.php
+++ prova/index.php
@@ -7,7 +7,13 @@ session_start();
     <title> Pagina iniziale </title>
     <link rel="stylesheet" href="stile_index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script>
+      $(document).ready(function(){
+        $("#nome_utente").load("nome_utente.php",);
+      });
+    </script>
    </head>
 <body>
   <header>
@@ -26,7 +32,7 @@ session_start();
       </div>
     </nav>
     <div class="text-content">
-      <h2>Learn To Enjoy,<br>Every Moment Of Your Life</h2>
+      <h2>Ti diamo il benvenuto,<br><span id="nome_utente"></span></h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum facere in nam, officiis aspernatur consectetur aliquid sequi possimus et. Sint.</p>
     </div>
   </header>
