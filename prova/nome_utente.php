c0819a3c8a24b4eae8bf8220104bf1089037f7de
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-09-13 13:04:18 +0200

    Add files via upload

---
+++ prova/nome_utente.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+include_once("connect.php");
+if (!isset($_SESSION["session_utente"])) {
+	echo"Session unset";
+	exit;
+} else {
+	$id_utente = $_SESSION["session_utente"];
+	$query_nome = mysqli_query($link,"SELECT Username FROM utente WHERE IdUtente=$id_utente");
+	$risultato = mysqli_fetch_assoc($query_nome);
+	echo $risultato['Username'];
+}
+?>
