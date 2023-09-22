abd0da4f6d12ef4ac0323bc6c4214b7cebc38a56
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-09-22 13:00:54 +0200

    Add files via upload

---
+++ prova/categorie.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+include_once("connect.php");
+if (!isset($_SESSION["session_utente"])) {
+    echo "Sessione annullata";
+    exit;
+} else {
+    $query_categorie = mysqli_query($link,"SELECT Icona, Nome FROM categoria WHERE IdCategoria NOT IN (SELECT IdSottocategoria FROM contiene) LIMIT 7");
+    $html = "";
+    while($row = mysqli_fetch_assoc($query_categorie)){
+        $iconData = $row['Icona'];
+        $base64Icon = base64_encode($iconData);
+        $categoryName = $row['Nome'];
+        $html .= "<div class='macrocat'>";
+        $html .= "<img src='data:image/png;base64, $base64Icon' alt='$categoryName'></img>";
+        $html .= "<p>$categoryName</p>";
+        $html .= "</div>";
+    }
+    $html .= "</div>";
+    echo $html;
+}
+?>
