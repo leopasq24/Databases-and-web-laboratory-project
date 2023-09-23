a087a0834db10d4a069f14905631db0e6e6be815
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-09-23 18:11:08 +0200

    Update loggato.php

--- prova/loggato.php
+++ prova/loggato.php
@@ -23,22 +23,32 @@ if($i==0){
 	$result_utente_passw=mysqli_stmt_get_result($stmt_utente_passw);
 	$result_array = mysqli_fetch_row($result_utente_passw);
 	if(empty($result_array)){
-		echo("Username non valido");
+		echo("Credenziali errate");
 		$i=$i+1;
 		mysqli_stmt_close($stmt_utente_passw);
 	}
 	elseif($result_array[2]!=$pass){
-		echo("Password errata");
+		echo("Credenziali errate");
 		$i=$i+1;
 		mysqli_stmt_close($stmt_utente_passw);
 	}
 	else{
-		$_SESSION['session_utente']=$result_array[0];
+		$_SESSION["session_utente"]=$result_array[0];
 	}
 }
 
 if($i==0){
-	echo "OK";
+	$stmt_primo_accesso=mysqli_prepare($link, "SELECT IdUtente FROM blog WHERE IdUtente=? UNION SELECT IdUtente FROM post WHERE IdUtente=? UNION SELECT IdUtente FROM feedback WHERE idUtente=? UNION SELECT IdUtente FROM commenta WHERE idUtente=?");
+	mysqli_stmt_bind_param($stmt_primo_accesso,"iiii",$_SESSION["session_utente"],$_SESSION["session_utente"],$_SESSION["session_utente"],$_SESSION["session_utente"]);
+	mysqli_stmt_execute($stmt_primo_accesso);
+	$result_primo_accesso = mysqli_stmt_get_result($stmt_primo_accesso);
+	$result_array = mysqli_fetch_row($result_primo_accesso);
+	if(empty($result_array)){
+		echo("Primo accesso");
+	}
+	else{
+		echo("OK");
+	}
 }
 
 ?>
