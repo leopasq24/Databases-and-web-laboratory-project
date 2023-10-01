785e59b472350208120b360536b3ac8f1bea3247
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-10-01 18:27:13 +0200

    Update categorie_select.php

--- prova/categorie_select.php
+++ prova/categorie_select.php
@@ -6,11 +6,19 @@ if (!isset($_SESSION["session_utente"])) {
     exit;
 } else {
     $query_categorie = mysqli_query($link,"SELECT Icona, Nome FROM categoria WHERE IdCategoria NOT IN (SELECT IdSottocategoria FROM contiene)");
-    $html = "";
-    while($row = mysqli_fetch_assoc($query_categorie)){
-        $categoryName = $row['Nome'];
+    $query_sottocategorie = mysqli_query($link,"SELECT Icona, Nome FROM categoria WHERE IdCategoria IN (SELECT IdSottocategoria FROM contiene)");
+    $html = "<optgroup label='Categorie'>";
+    while($row_cat = mysqli_fetch_assoc($query_categorie)){
+        $categoryName = $row_cat['Nome'];
         $html .= "<option value='{$categoryName}'>{$categoryName}</option>";
     }
+    $html .="</optgroup>";
+    $html .= "<optgroup label='Sottocategorie'>";
+    while($row_subcat = mysqli_fetch_assoc($query_sottocategorie)){
+        $subcategoryName = $row_subcat['Nome'];
+        $html .= "<option value='{$subcategoryName}'>{$subcategoryName}</option>";
+    }
+    $html .="</optgroup>";
     echo $html;
 }
 ?>
