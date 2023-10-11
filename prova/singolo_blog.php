beb1b530781f1ff275da195773e08e194c096e8a
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-10-11 10:55:51 +0200

    Update singolo_blog.php

--- prova/singolo_blog.php
+++ prova/singolo_blog.php
@@ -1,46 +1,52 @@
 <?php
 session_start();
+include_once("connect.php");
+
 $idBlog = $_GET['idBlog'];
 $Blog_title = $_GET['Blog_title'];
-if (isset($_GET["idBlog"])){
+if (!isset($_SESSION["session_utente"])) {
+    echo "Sessione annullata";
+    exit;
+}else if (isset($_GET["idBlog"])){
   $id_utente = $_SESSION["session_utente"];
   $stmt_info_blog = mysqli_prepare($link, "SELECT blog.Immagine, blog.Titolo, categoria.Nome, blog.Descrizione FROM blog, categoria WHERE blog.IdUtente=? AND blog.IdBlog=? AND blog.IdCategoria = categoria.IdCategoria");
   mysqli_stmt_bind_param($stmt_info_blog, "ii", $id_utente, $idBlog);
   mysqli_stmt_execute($stmt_info_blog);
   $query_info_blog = mysqli_stmt_get_result($stmt_info_blog);
   $html = "";
-  $html .= "<div class='info-blog'>";
+  $html .= "<div class='dati-blog'>";
   while ($row = mysqli_fetch_assoc($query_info_blog)) {
-  $imageData = $row['Immagine'];
-  $titolo = $row['Titolo'];
-  $categoria = $row['Nome'];
-  $descrizione = $row['Descrizione'];
-  if ($imageData == null) {
-      $src_img = "foto/blog.png";
-  } else {
-      $base64Image = base64_encode($imageData);
-      $src_img = "data:image/png;base64," . $base64Image;
+    $imagePath = $row['Immagine'];
+    $titolo = $row['Titolo'];
+    $categoria = $row['Nome'];
+    $descrizione = $row['Descrizione'];
+    if ($imagePath == null) {
+      $imagePath = "foto/blog.png";
+    }
+    $html .= "<img src='{$imagePath}' alt='{$titolo}'></img>";
+    $html .= "<p class='titolo_blog'>{$titolo}</p>";
+    $html .= "<p class='cat_blog'>{$categoria}</p>";
+    $html .= "<p class='descrizione_blog'>{$descrizione}</p>";
   }
-  $html .= "<img src='{$src_img}' alt='{$titolo}'></img>";
-  $html .= "<p class='titolo_blog'>{$titolo}</p>";
-  $html .= "<p class='cat_blog'>{$categoria}</p>";
-  $html .= "<p class='descrizione_blog'>{$descrizione}</p>";
+  $stmt_proprietario = mysqli_prepare($link, "SELECT IdUtente FROM blog WHERE IdBlog=?");
+  mysqli_stmt_bind_param($stmt_proprietario, "i", $idBlog);
+  mysqli_stmt_execute($stmt_proprietario);
+  $query_proprietario = mysqli_stmt_get_result($stmt_proprietario);
+  while ($row = mysqli_fetch_assoc($query_proprietario)){
+    if ($row["IdUtente"]==$id_utente){
+      $html .= "<input type='button' class='modifica_blog' value='Modifica'>
+      <input type='button' class='elimina_blog' value='Elimina'>";
+    }
+  }
+  mysqli_stmt_close($stmt_info_blog);
   $html .= "</div>";
 }
-else {
-  echo "Errore";
-}
-mysqli_stmt_close($stmt_info_blog);
-}
-$html .= "</div>";
 ?>
     <input type="button" id="crea_post" value="Crea un nuovo post">
     <div class="grid-post">
       <div class="colonna-blog">
         <p class="titolo">Info e gestione blog</p>
         <div class="info-blog"><?php echo $html ?></div>
-        <input type="button" id="modifica_blog" value="Modifica">
-        <input type="button" id="elimina_blog" value="Elimina">
       </div>
       <div class="post">
         <p class="titolo">I post di <?php echo $Blog_title ?></br>
