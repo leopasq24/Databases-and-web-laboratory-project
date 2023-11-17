eba5d5bcaf29565c04e4e7a2cd908383fe3e87b9
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-11-17 15:05:31 +0100

    Update tutti_i_blog_select.php

--- prova/tutti_i_blog_select.php
+++ prova/tutti_i_blog_select.php
@@ -16,11 +16,11 @@ if (!isset($_SESSION["session_utente"])) {
     $tipo = isset($_GET['tipo']) ? $_GET['tipo'] : "Recenti";
 
     if ($tipo === "Recenti") {
-        $stmt_tutti_i_blog = mysqli_prepare($link, "SELECT IdBlog, Titolo, Descrizione, Immagine FROM blog WHERE IdUtente != ? ORDER BY IdBlog DESC LIMIT ?, 9");
+        $stmt_tutti_i_blog = mysqli_prepare($link, "SELECT IdBlog, Titolo, Descrizione, Immagine, Username FROM blog, utente WHERE blog.IdUtente != ? AND blog.IdUtente=utente.IdUtente ORDER BY IdBlog DESC LIMIT ?, 9");
     } elseif ($tipo === "Popolari") {
-        $stmt_tutti_i_blog = mysqli_prepare($link, "SELECT IdBlog, Titolo, Descrizione, Immagine FROM blog WHERE IdUtente != ? AND IdBlog IN (SELECT IdBlog FROM post WHERE IdPost IN (SELECT codice FROM post_popolari ORDER BY conta DESC)) LIMIT ?, 9");
+        $stmt_tutti_i_blog = mysqli_prepare($link, "SELECT IdBlog, Titolo, Descrizione, Immagine, Username FROM blog, utente WHERE blog.IdUtente != ? AND blog.IdUtente=utente.IdUtente AND IdBlog IN (SELECT IdBlog FROM post WHERE IdPost IN (SELECT codice FROM post_popolari ORDER BY conta DESC)) LIMIT ?, 9");
     } elseif ($tipo === "Autore") {
-        $stmt_tutti_i_blog = mysqli_prepare($link, "SELECT IdBlog, Titolo, Descrizione, Immagine FROM blog WHERE IdUtente != ? ORDER BY IdUtente LIMIT ?, 9");
+        $stmt_tutti_i_blog = mysqli_prepare($link, "SELECT IdBlog, Titolo, Descrizione, Immagine, Username FROM blog, utente WHERE blog.IdUtente != ? AND blog.IdUtente=utente.IdUtente ORDER BY utente.Username ASC LIMIT ?, 9");
     }
     mysqli_stmt_bind_param($stmt_tutti_i_blog, "ii", $id_utente, $numeroBlog);
     mysqli_stmt_execute($stmt_tutti_i_blog);
@@ -34,19 +34,18 @@ if (!isset($_SESSION["session_utente"])) {
     } else {
         while ($row = mysqli_fetch_assoc($query_tutti_i_blog)) {
             $idblog = $row['IdBlog'];
-            $imageData = $row['Immagine'];
+            $src_img = $row['Immagine'];
             $descrizione = $row['Descrizione'];
             $Title = $row['Titolo'];
-            if ($imageData == null) {
+            $autore = $row['Username'];
+            if ($src_img == null) {
                 $src_img = "foto/blog.png";
-            } else {
-                $base64Image = base64_encode($imageData);
-                $src_img = "data:image/png;base64," . $base64Image;
             }
             $html .= "<div class='blog' data-blog-title='{$Title}' data-blog-id='{$idblog}'>";
             $html .= "<img src='{$src_img}' alt='{$Title}'></img>";
             $html .= "<p class='titolo_tuoi_blog'>{$Title}</p>";
             $html .= "<p class='descrizione_tuoi_blog'>{$descrizione}</p>";
+            $html .= "<p class='autore_tuoi_blog'>{$autore}</p>";
             $html .= "</div>";
         }
         mysqli_stmt_close($stmt_tutti_i_blog);
