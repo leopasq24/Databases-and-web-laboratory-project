5a4b0ea667fbc7313b484f53010569ee17300e64
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-11-17 15:02:33 +0100

    Update sottocategorie.php

--- prova/sottocategorie.php
+++ prova/sottocategorie.php
@@ -6,15 +6,22 @@ if (!isset($_SESSION["session_utente"])) {
     exit;
 } else {
     $nome_cat = $_GET['key1'];
-    $query_sottocategorie = mysqli_query($link,"SELECT Nome FROM categoria, contiene WHERE categoria.IdCategoria = contiene.IdSottocategoria AND contiene.IdSopracategoria = (SELECT IdCategoria FROM categoria WHERE Nome = '$nome_cat')");
     $html = "";
-    while($row = mysqli_fetch_assoc($query_sottocategorie)){
+    $stmt_subcategories = mysqli_prepare($link, "SELECT Nome FROM categoria, contiene WHERE categoria.IdCategoria = contiene.IdSottocategoria AND contiene.IdSopracategoria = (SELECT IdCategoria FROM categoria WHERE Nome = ?)");
+    
+    mysqli_stmt_bind_param($stmt_subcategories, "s", $nome_cat);
+    mysqli_stmt_execute($stmt_subcategories);
+    $result_subcategories = mysqli_stmt_get_result($stmt_subcategories);
+    
+    while ($row = mysqli_fetch_assoc($result_subcategories)) {
         $subcategoryName = $row['Nome'];
         $html .= "<div class='microcat'>";
         $html .= "<p>$subcategoryName</p>";
         $html .= "</div>";
     }
+    
     $html .= "</div>";
-    echo $html;
+    mysqli_stmt_close($stmt_subcategories);
+    echo $html;  
 }
 ?>
