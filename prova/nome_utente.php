a00155907fa6ff2c7c5f8d98371bbdd0ccb1ff12
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-11-17 14:58:53 +0100

    Update nome_utente.php

--- prova/nome_utente.php
+++ prova/nome_utente.php
@@ -2,12 +2,20 @@
 session_start();
 include_once("connect.php");
 if (!isset($_SESSION["session_utente"])) {
-	echo"Sessione annullata";
+	echo "Sessione annullata";
 	exit;
 } else {
 	$id_utente = $_SESSION["session_utente"];
-	$query_nome = mysqli_query($link,"SELECT Username FROM utente WHERE IdUtente=$id_utente");
-	$risultato = mysqli_fetch_assoc($query_nome);
-	echo $risultato['Username'];
+
+	$stmt = mysqli_prepare($link, "SELECT Username FROM utente WHERE IdUtente = ?");
+	mysqli_stmt_bind_param($stmt, "i", $id_utente);
+	mysqli_stmt_execute($stmt);
+	mysqli_stmt_bind_result($stmt, $username);
+	if (mysqli_stmt_fetch($stmt)) {
+    	echo $username;
+	} else {
+    	echo "Utente non trovato";
+	}
+	mysqli_stmt_close($stmt);
 }
 ?>
