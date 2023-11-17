7bf1fe943eebac20fd6b88e4865593dcaad3a2f0
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-11-17 14:51:34 +0100

    Update categorie_select.php

--- prova/categorie_select.php
+++ prova/categorie_select.php
@@ -5,20 +5,28 @@ if (!isset($_SESSION["session_utente"])) {
     echo "Sessione annullata";
     exit;
 } else {
-    $query_categorie = mysqli_query($link,"SELECT Icona, Nome FROM categoria WHERE IdCategoria NOT IN (SELECT IdSottocategoria FROM contiene)");
-    $query_sottocategorie = mysqli_query($link,"SELECT Icona, Nome FROM categoria WHERE IdCategoria IN (SELECT IdSottocategoria FROM contiene)");
     $html = "<optgroup label='Categorie'>";
-    while($row_cat = mysqli_fetch_assoc($query_categorie)){
-        $categoryName = $row_cat['Nome'];
+    $stmt_categories = mysqli_prepare($link, "SELECT Icona, Nome FROM categoria WHERE IdCategoria NOT IN (SELECT IdSottocategoria FROM contiene)");
+    mysqli_stmt_execute($stmt_categories);
+    $result_categories = mysqli_stmt_get_result($stmt_categories);
+
+    while ($row_cat = mysqli_fetch_assoc($result_categories)) {
+        $categoryName = htmlspecialchars($row_cat['Nome'], ENT_QUOTES, 'UTF-8');
         $html .= "<option value='{$categoryName}'>{$categoryName}</option>";
     }
-    $html .="</optgroup>";
+    mysqli_stmt_close($stmt_categories);
+    $html .= "</optgroup>";
     $html .= "<optgroup label='Sottocategorie'>";
-    while($row_subcat = mysqli_fetch_assoc($query_sottocategorie)){
-        $subcategoryName = $row_subcat['Nome'];
+    $stmt_subcategories = mysqli_prepare($link, "SELECT Icona, Nome FROM categoria WHERE IdCategoria IN (SELECT IdSottocategoria FROM contiene)");
+    mysqli_stmt_execute($stmt_subcategories);
+    $result_subcategories = mysqli_stmt_get_result($stmt_subcategories);
+
+    while ($row_subcat = mysqli_fetch_assoc($result_subcategories)) {
+        $subcategoryName = htmlspecialchars($row_subcat['Nome'], ENT_QUOTES, 'UTF-8');
         $html .= "<option value='{$subcategoryName}'>{$subcategoryName}</option>";
     }
-    $html .="</optgroup>";
+    mysqli_stmt_close($stmt_subcategories);
+    $html .= "</optgroup>";
     echo $html;
 }
 ?>
