492ef1627fb4bbe240b87ac7cffd80c216e29812
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-11-17 14:50:15 +0100

    Update blog.php

--- prova/blog.php
+++ prova/blog.php
@@ -14,19 +14,16 @@ if (!isset($_SESSION["session_utente"])) {
     $html .="<p class='presentazione'>I Blog creati da te:</p>";
     $html .= "<div class='griglia_blog_creati'>";
     if (mysqli_num_rows($query_tuoi_blog) === 0) {
-        $html .= "<p id='nessun_blog'>Ops! Nessun blog nei paraggi... </br> Che ne dici di creane uno? ;)</p>";
+        $html .= "<p id='nessun_blog'><img src='foto/bolle.png' alt='bolle'></img></br>Ops! Nessun blog nei paraggi... </br> Aspetta che qualcuno ti renda coautore del suo blog! </p>";
     }
     else{
         while ($row = mysqli_fetch_assoc($query_tuoi_blog)) {
             $idblog = $row['IdBlog'];
-            $imageData = $row['Immagine'];
+            $src_img = $row['Immagine'];
             $descrizione = $row['Descrizione'];
             $Title = $row['Titolo'];
-            if ($imageData == null) {
+            if ($src_img == null) {
                 $src_img = "foto/blog.png";
-            } else {
-                $base64Image = base64_encode($imageData);
-                $src_img = "data:image/png;base64," . $base64Image;
             }
             $html .= "<div class='blog' data-blog-title='{$Title}' data-blog-id='{$idblog}'>";
             $html .= "<img src='{$src_img}' alt='{$Title}'></img>";
@@ -37,30 +34,28 @@ if (!isset($_SESSION["session_utente"])) {
         mysqli_stmt_close($stmt_tuoi_blog);
     }
     $html .= "</div>";
-    $stmt_blog_coaut = mysqli_prepare($link, "SELECT Titolo, Descrizione, Immagine FROM blog WHERE IdBlog IN (SELECT IdBlog FROM coautore WHERE IdUtente=?)");
+    $stmt_blog_coaut = mysqli_prepare($link, "SELECT IdBlog, Titolo, Descrizione, Immagine FROM blog WHERE IdBlog IN (SELECT IdBlog FROM coautore WHERE IdUtente=?)");
     mysqli_stmt_bind_param($stmt_blog_coaut, "i", $id_utente);
     mysqli_stmt_execute($stmt_blog_coaut);
     $query_blog_coaut = mysqli_stmt_get_result($stmt_blog_coaut);
     $html .="<p class='presentazione_coautore'>I Blog di cui sei coautore:</p>";
     $html .= "<div class='griglia_blog_coautore'>";
     if (mysqli_num_rows($query_blog_coaut) === 0) {
-        $html .= "<p id='nessun_blog'>Ops! Nessun blog nei paraggi... </br> Aspetta che qualcuno ti renda coautore del suo blog! </p>";
+        $html .= "<p id='nessun_blog'><img src='foto/bolle.png' alt='bolle'></img></br>Ops! Nessun blog nei paraggi... </br> Aspetta che qualcuno ti renda coautore del suo blog! </p>";
     }
     else{
         while ($row = mysqli_fetch_assoc($query_blog_coaut)) {
-            $imageData = $row['Immagine'];
+            $idblog = $row['IdBlog'];
+            $src_img = $row['Immagine'];
             $descrizione = $row['Descrizione'];
             $Title = $row['Titolo'];
-            if ($imageData == null) {
+            if ($src_img == null) {
                 $src_img = "foto/blog.png";
-            } else {
-                $base64Image = base64_encode($imageData);
-                $src_img = "data:image/png;base64," . $base64Image;
             }
-            $html .= "<div class='blog'>";
+            $html .= "<div class='blog' data-blog-title='{$Title}' data-blog-id='{$idblog}'>";
             $html .= "<img src='{$src_img}' alt='{$Title}'></img>";
-            $html .= "<p>{$Title}</p>";
-            $html .= "<p>{$descrizione}</p>";
+            $html .= "<p class='titolo_tuoi_blog'>{$Title}</p>";
+            $html .= "<p class='descrizione_tuoi_blog'>{$descrizione}</p>";
             $html .= "</div>";
         }
         mysqli_stmt_close($stmt_blog_coaut);
