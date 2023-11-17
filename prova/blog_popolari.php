7deee53c31ba03b0963e7d79d899973443b6c39f
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-11-17 14:50:46 +0100

    Update blog_popolari.php

--- prova/blog_popolari.php
+++ prova/blog_popolari.php
@@ -1,29 +1,34 @@
 <?php
 session_start();
 include_once("connect.php");
+
 if (!isset($_SESSION["session_utente"])) {
     echo "Sessione annullata";
     exit;
 } else {
-    $query_blog_popolari = mysqli_query($link,"SELECT Immagine, Titolo FROM blog WHERE IdBlog IN (SELECT IdBlog FROM post WHERE IdPost IN (SELECT codice FROM post_popolari ORDER BY conta DESC)) LIMIT 7");
     $html = "<div class='blog-pop'>";
-    while($row = mysqli_fetch_assoc($query_blog_popolari)){
+    $stmt_popular_blogs = mysqli_prepare($link, "SELECT Immagine, Titolo FROM blog WHERE IdBlog IN (SELECT IdBlog FROM post WHERE IdPost IN (SELECT codice FROM post_popolari ORDER BY conta DESC)) LIMIT 7");
+    
+    mysqli_stmt_execute($stmt_popular_blogs);
+    $result_popular_blogs = mysqli_stmt_get_result($stmt_popular_blogs);
+    
+    while ($row = mysqli_fetch_assoc($result_popular_blogs)) {
         $imageData = $row['Immagine'];
-        if($imageData='NULL'){
-            $src_img="foto/blog.png";
+        if ($imageData === null) {
+            $src_img = "foto/blog.png";
+        }else{
+            $src_img = $imageData;
         }
-        else{
-            $base64Image = base64_encode($imageData);
-            $src_img="data:image/png;base64".$base64Image;
-        }
-        $base64Image = base64_encode($imageData);
         $Title = $row['Titolo'];
+        
         $html .= "<div class='blog-pop'>";
-        $html .= "<img src={$src_img} alt='$Title'></img>";
-        $html .= "<p>$Title</p>";
+        $html .= "<img src='{$src_img}' alt='{$Title}'></img>";
+        $html .= "<p>{$Title}</p>";
         $html .= "</div>";
     }
+    
     $html .= "</div>";
+    mysqli_stmt_close($stmt_popular_blogs);
     echo $html;
 }
 ?>
