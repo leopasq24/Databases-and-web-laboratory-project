52769a3b7b6e4c478135a32f400d75d42d16ae1e
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2023-11-17 14:59:17 +0100

    Update post.php

--- prova/post.php
+++ prova/post.php
@@ -5,45 +5,95 @@ if (!isset($_SESSION["session_utente"])) {
     echo "Sessione annullata";
     exit;
 } else {
-    $query_post = mysqli_query($link,"SELECT post.Titolo AS Titolo_post, Testo, Data, Ora, post.Immagine AS immagine_post, blog.Titolo AS Argomento, blog.Immagine AS immagine_blog, Username FROM post, blog, utente WHERE post.IdBlog=blog.IdBlog AND post.IdUtente=utente.IdUtente ORDER BY Data DESC LIMIT 7");
     $html = "";
-    while($row = mysqli_fetch_assoc($query_post)){
-    	$image_post_Data = $row['immagine_post'];
-    	$title = $row['Titolo_post'];
-        $testo = $row['Testo'];
-        $autore_post = $row['Username'];
-        $blog = $row['Argomento'];
-        $data = $row['Data'];
-        $ora = $row['Ora'];
-        $image_blog_Data = $row['immagine_blog'];
-        if($image_blog_Data='NULL'){
-            $src_img="foto/blog.png";
-        }
-        else{
-            $base64Image_blog_Data = base64_encode($image_blog_Data);
-            $src_img="data:image/png;base64".$base64Image_blog_Data;
-        }    
-    	if($imageData='NULL'){
-    		$html .= "<div class='nuovo-post'>";
-    		$html .= "<img src={$src_img} alt='$blog'></img>";
-    		$html .= "<p>$blog</p>";
-        	$html .= "<h4>$title</h4>";
-        	$html .= "<p>$testo</p>";
-        	$html .= "<p class='dataeora'>$data $ora</p>";
-        	$html .= "</div>";
+    $stmt_posts = mysqli_prepare($link, "SELECT IdPost, post.Titolo AS Titolo_post, Testo, Data, Ora, post.Immagine AS immagine_post, blog.Titolo AS Argomento, blog.Immagine AS immagine_blog, Username FROM post, blog, utente WHERE post.IdBlog=blog.IdBlog AND post.IdUtente=utente.IdUtente ORDER BY Data DESC LIMIT 7");
+    
+    if(mysqli_stmt_execute($stmt_posts)){
+    	$result_posts = mysqli_stmt_get_result($stmt_posts);
+
+    	if (mysqli_num_rows($result_posts) == 0) {
+        	$html .= "<p class='nessun_post'>Nessun post</p>";
+    	}else{
+    		while ($row = mysqli_fetch_assoc($result_posts)) {
+    			$id_post = $row['IdPost'];
+    			$img_blog = $row['immagine_blog'];
+    			$img_post = $row['immagine_post'];
+        		$title = $row['Titolo_post'];
+        		$testo = $row['Testo'];
+        		$autore_post = $row['Username'];
+        		$blog = $row['Argomento'];
+        		$data = $row['Data'];
+        		$ora = $row['Ora'];
+        		if ($img_blog == null) {
+            		$src_img = "foto/blog.png";
+        		} else {
+            		$src_img = $img_blog;
+        		}
+        		
+        		$stmt_feedback_positivi = mysqli_prepare($link, "SELECT numerofeedbackpositivi FROM numero_feedback_positivi WHERE IdPost=?");
+        		mysqli_stmt_bind_param($stmt_feedback_positivi,"i", $id_post);
+				mysqli_stmt_execute($stmt_feedback_positivi);
+				$results_feedback_positivi = mysqli_stmt_get_result($stmt_feedback_positivi);
+				if (mysqli_num_rows($results_feedback_positivi) == 0) {
+        			$num_feedback_positivi=0;
+        		}else{
+        			while ($row = mysqli_fetch_assoc($results_feedback_positivi)){
+        				$num_feedback_positivi=$row['numerofeedbackpositivi'];
+        			}
+        		}
+
+        		$stmt_feedback_negativi = mysqli_prepare($link, "SELECT numerofeedbacknegativi FROM numero_feedback_negativi WHERE IdPost=?");
+        		mysqli_stmt_bind_param($stmt_feedback_negativi,"i", $id_post);
+				mysqli_stmt_execute($stmt_feedback_negativi);
+				$results_feedback_negativi = mysqli_stmt_get_result($stmt_feedback_negativi);
+				if (mysqli_num_rows($results_feedback_negativi) == 0) {
+        			$num_feedback_negativi=0;
+        		}else{
+        			while ($row = mysqli_fetch_assoc($results_feedback_negativi)){
+        				$num_feedback_negativi=$row['numerofeedbacknegativi'];
+        			}
+        		}
+
+        		$stmt_numero_commenti = mysqli_prepare($link, "SELECT numerocommenti FROM numero_commenti WHERE IdPost=?");
+        		mysqli_stmt_bind_param($stmt_numero_commenti,"i", $id_post);
+				mysqli_stmt_execute($stmt_numero_commenti);
+				$stmt_numero_commenti = mysqli_stmt_get_result($stmt_numero_commenti);
+				if (mysqli_num_rows($stmt_numero_commenti) == 0) {
+        			$numero_commenti=0;
+        		}else{
+        			while ($row = mysqli_fetch_assoc($stmt_numero_commenti)){
+        				$numero_commenti=$row['numerocommenti'];
+        			}
+        		}
+
+       	 		if ($img_post === null) {
+            		$html .= "<div class='nuovo-post'>";
+            		$html .= "<img src='{$src_img}' alt='{$blog}'></img>";
+            		$html .= "<p>{$blog}</p>";
+            		$html .= "<h4>{$title}</h4>";
+            		$html .= "<p>{$testo}</p>";
+            		$html .= "<p class='dataeora'><span>{$autore_post}</span> {$data} {$ora}</p>";
+            		$html .= "<p class='feedback_e_commenti'>{$num_feedback_positivi} {$num_feedback_negativi} {$numero_commenti}</p>";
+            		$html .= "</div>";
+        		} else {
+            		$html .= "<div class='nuovo-post'>";
+            		$html .= "<img src='{$src_img}' alt='{$blog}'></img>";
+            		$html .= "<p>{$blog}</p>";
+            		$html .= "<h4>{$title}</h4>";
+            		$html .= "<img src='{$img_post}' alt='{$title}'></img>";
+            		$html .= "<p>{$testo}</p>";
+            		$html .= "<p class='dataeora'><span>{$autore_post}</span> {$data} {$ora}</p>";
+            		$html .= "<p class='feedback_e_commenti'>{$num_feedback_positivi} {$num_feedback_negativi} {$numero_commenti}</p>";
+            		$html .= "</div>";
+        		}
+    		}
     	}
-    	else{
-    		$base64Image_post_Data = base64_encode($image_post_Data);
-        	$html .= "<div class='nuovo-post'>";
-        	$html .= "<p>$blog</p>";
-        	$html .= "<h4>$title</h4>";
-        	$html .= "<img src='data:image/png;base64, $base64Image_post_Data' alt='$title'></img>";
-        	$html .= "<p>$testo</p>";
-        	$html .= "<p class='dataeora'>$data $ora</p>";
-        	$html .= "</div>";
-    	}
+    	
+    }else{
+    	$html .="<p>Errore nel DataBase</p>";
     }
-    $html .= "</div>";
+    
+    mysqli_stmt_close($stmt_posts);
     echo $html;
 }
 ?>
