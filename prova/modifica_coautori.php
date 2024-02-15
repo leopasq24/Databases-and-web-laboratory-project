a0afc8c28cd1a1671d12cc9b64648a50d2938cd2
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2024-02-15 18:05:03 +0100

    Update modifica_coautori.php

--- prova/modifica_coautori.php
+++ prova/modifica_coautori.php
@@ -1,10 +1,12 @@
 <?php
 include_once("connect.php");
 session_start();
-if(isset($_POST["coautori"]) && isset($_POST["idblog"])){
+if(isset($_POST["coautori"]) && isset($_POST["idblo"])){
 	$idblog=$_POST["idblog"];
 	$coautori = explode(", ", trim($_POST["coautori"]));
 	$a=array();
+	$updatedData=array();
+	$nuovi_coautori=array();
 
 	foreach ($coautori as $key => $value){
 		$stmt_idcoautori=mysqli_prepare($link, "SELECT IdUtente FROM utente WHERE username=?");
@@ -28,10 +30,23 @@ if(isset($_POST["coautori"]) && isset($_POST["idblog"])){
            mysqli_stmt_execute($stmt_coautori);
            mysqli_stmt_close($stmt_coautori);
         }
-        
-    echo"OK";
+
+    $stmt_nuovi_coautori = mysqli_prepare($link, "SELECT Username FROM coautore, utente WHERE coautore.IdUtente=utente.IdUtente AND coautore.IdBlog=?");
+    mysqli_stmt_bind_param($stmt_nuovi_coautori, "i", $idblog);
+    mysqli_stmt_execute($stmt_nuovi_coautori);
+    $result_nuovi_coautori=mysqli_stmt_get_result($stmt_nuovi_coautori);
+    while($row=mysqli_fetch_assoc($result_nuovi_coautori)){
+    	array_push($nuovi_coautori, $row["Username"]);
+    }
+    mysqli_stmt_close($stmt_nuovi_coautori);
+
+    $output = implode(", ", $nuovi_coautori);
+
+	echo json_encode(array('status' => 'OK', 'data' => $output));
+
 }else {
-    	echo "richiesta fallita:".var_dump($_POST);
-	}
+	$response = "Richiesta fallita";
+    echo json_encode(array('status' => 'Errore', 'data' => $response));
+}
 
 ?>
