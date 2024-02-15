b5b0641a20d060a60d9980bfbd3230c4318a8174
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2024-02-15 17:51:55 +0100

    Update categorie.php

--- prova/categorie.php
+++ prova/categorie.php
@@ -7,25 +7,26 @@ if (!isset($_SESSION["session_utente"])) {
     exit;
 } else {
     $html = "";
-    $stmt_categories = mysqli_prepare($link, "SELECT Icona, Nome FROM categoria WHERE IdCategoria NOT IN (SELECT IdSottocategoria FROM contiene) LIMIT 7");
+    $stmt_categorie = mysqli_prepare($link, "SELECT IdCategoria, Icona, Nome FROM categoria WHERE IdCategoria NOT IN (SELECT IdSottocategoria FROM contiene) LIMIT 7");
     
-    mysqli_stmt_execute($stmt_categories);
-    $result_categories = mysqli_stmt_get_result($stmt_categories);
+    mysqli_stmt_execute($stmt_categorie);
+    $result_categorie = mysqli_stmt_get_result($stmt_categorie);
     
-    while ($row = mysqli_fetch_assoc($result_categories)) {
+    while ($row = mysqli_fetch_assoc($result_categorie)) {
         $iconData = $row['Icona'];
         $base64Icon = base64_encode($iconData);
-        $categoryName = $row['Nome'];
-        
-        $html .= "<div class='macrocat'>";
-        $html .= "<img src='data:image/png;base64, $base64Icon' alt='$categoryName'></img>";
-        $html .= "<p id='macrocat_nome'>{$categoryName}</p><p id='freccia'>&#8250;</p>";
+        $nome_categoria = $row['Nome'];
+        $id_categoria =  $row['IdCategoria'];
+
+        $html .= "<div class='macrocat' data-cat-id='$id_categoria'>";
+        $html .= "<img src='data:image/png;base64, $base64Icon' alt='$nome_categoria'></img>";
+        $html .= "<p id='macrocat_nome'>{$nome_categoria}</p><p id='freccia'>&#8250;</p>";
         $html .= "<div class='micro-categoria'></div>";
         $html .= "</div>";
     }
     
     $html .= "</div>";
-    mysqli_stmt_close($stmt_categories);
+    mysqli_stmt_close($stmt_categorie);
     echo $html;   
 }
 ?>
