cd6b460f01e75fdd839521840632ae8cfd99de50
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2024-03-03 18:16:34 +0100

    Update sottocategorie.php

--- prova/sottocategorie.php
+++ prova/sottocategorie.php
@@ -5,24 +5,29 @@ if (!isset($_SESSION["session_utente"])) {
     echo "Sessione annullata";
     exit;
 } else {
-    $nome_cat = $_GET['key1'];
-    $html = "";
-    $stmt_subcategories = mysqli_prepare($link, "SELECT IdCategoria, Nome FROM categoria, contiene WHERE categoria.IdCategoria = contiene.IdSottocategoria AND contiene.IdSopracategoria = (SELECT IdCategoria FROM categoria WHERE Nome = ?)");
+    if(isset( $_GET['key1'])){
+        $id_cat = $_GET['key1'];
+        $html = "";
+        $stmt_sottocategorie = mysqli_prepare($link, "SELECT IdCategoria, Nome FROM categoria, contiene WHERE categoria.IdCategoria = contiene.IdSottocategoria AND contiene.IdSopracategoria = ?");
     
-    mysqli_stmt_bind_param($stmt_subcategories, "s", $nome_cat);
-    mysqli_stmt_execute($stmt_subcategories);
-    $result_subcategories = mysqli_stmt_get_result($stmt_subcategories);
+        mysqli_stmt_bind_param($stmt_sottocategorie, "i", $id_cat);
+        mysqli_stmt_execute($stmt_sottocategorie);
+        $results_sottocategorie = mysqli_stmt_get_result($stmt_sottocategorie);
     
-    while ($row = mysqli_fetch_assoc($result_subcategories)) {
-        $subcategoryName = $row['Nome'];
-        $id_categoria =  $row['IdCategoria'];
-        $html .= "<div class='microcat' data-cat-id='$id_categoria'>";
-        $html .= "<p>$subcategoryName</p>";
+        while ($row = mysqli_fetch_assoc($results_sottocategorie)) {
+            $nome_cat = $row['Nome'];
+            $id_categoria =  $row['IdCategoria'];
+            $html .= "<div class='microcat' data-cat-id='$id_categoria'>";
+            $html .= "<p>$nome_cat</p>";
+            $html .= "</div>";
+        }
+    
         $html .= "</div>";
+        mysqli_stmt_close($stmt_sottocategorie);
+        echo $html; 
+    }else{
+        $html .= "<p>Categoria non specificata</p>";
+        echo $html;
     }
-    
-    $html .= "</div>";
-    mysqli_stmt_close($stmt_subcategories);
-    echo $html;  
 }
 ?>
