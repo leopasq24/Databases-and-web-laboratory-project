24c758b6ca6630dcf3dff15db4c16e3c2ec706bb
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2024-03-04 17:12:26 +0100

    Update ricerca.php

--- prova/ricerca.php
+++ prova/ricerca.php
@@ -4,7 +4,7 @@ $selezione = $_GET['selezione'];
 $str = trim($_GET['str']);
 if($selezione == "blog" and $str!=""){
     $new_str="%".$str."%";
-    $stmt= mysqli_prepare($link, "SELECT DISTINCT blog.IdBlog AS idblog, Titolo, Descrizione, Nome, Username FROM blog, categoria, utente WHERE blog.IdCategoria=categoria.IdCategoria AND blog.IdUtente=utente.IdUtente  AND (Titolo LIKE ? OR Descrizione LIKE ? OR Nome LIKE ? OR Username LIKE ?)");
+    $stmt= mysqli_prepare($link, "SELECT DISTINCT blog.IdBlog AS idblog, Titolo, Descrizione, Nome, Username FROM blog, categoria, utente WHERE blog.IdCategoria=categoria.IdCategoria AND blog.IdUtente=utente.IdUtente  AND (Titolo LIKE ? OR Descrizione LIKE ? OR Nome LIKE ? OR Username LIKE ?) LIMIT 4");
     mysqli_stmt_bind_param($stmt,"ssss", $new_str, $new_str, $new_str, $new_str);
 	mysqli_stmt_execute($stmt);
 	$results = mysqli_stmt_get_result($stmt);
@@ -42,7 +42,7 @@ if($selezione == "blog" and $str!=""){
 
 }else if($selezione == "post" and $str!=""){
     $new_str="%".$str."%";
-    $stmt= mysqli_prepare($link, "SELECT DISTINCT Titolo, Testo, Username FROM post, utente WHERE post.IdUtente=utente.IdUtente  AND (Titolo LIKE ? OR Testo LIKE ? OR Username LIKE ?)");
+    $stmt= mysqli_prepare($link, "SELECT DISTINCT Titolo, Testo, Username, IdBlog FROM post, utente WHERE post.IdUtente=utente.IdUtente  AND (Titolo LIKE ? OR Testo LIKE ? OR Username LIKE ?)LIMIT 4");
     mysqli_stmt_bind_param($stmt,"sss", $new_str, $new_str, $new_str);
 	mysqli_stmt_execute($stmt);
 	$results = mysqli_stmt_get_result($stmt);
@@ -61,10 +61,11 @@ if($selezione == "blog" and $str!=""){
         </thead>
         <tbody>";
         while($row = mysqli_fetch_assoc($results)){
+            $idBlog = $row['IdBlog'];
             $titolo_post = str_replace($str, $arr[$str], $row['Titolo']);
             $testo_post = str_replace($str, $arr[$str], $row['Testo']);
             $autore_post = str_replace($str, $arr[$str], $row['Username']);
-            $res.="<tr id='risultati'>
+            $res.="<tr id='risultati'  data-blog-id='{$idBlog}'>
                 <td class='titolo_post'>".$titolo_post."</td>
                 <td class='testo_post'>".$testo_post."</td>
                 <td class='autore_post'>".$autore_post."</td>
@@ -73,5 +74,59 @@ if($selezione == "blog" and $str!=""){
         $res.="</tbody></table>";
         echo $res;
     }
+}else if($selezione == "nuovi_risultati_blog" and $str!=""){
+    $n = $_GET['numero'];
+    $conta = 0;
+    $res = "";
+    $new_str="%".$str."%";
+    $stmt= mysqli_prepare($link, "SELECT DISTINCT blog.IdBlog AS idblog, Titolo, Descrizione, Nome, Username FROM blog, categoria, utente WHERE blog.IdCategoria=categoria.IdCategoria AND blog.IdUtente=utente.IdUtente  AND (Titolo LIKE ? OR Descrizione LIKE ? OR Nome LIKE ? OR Username LIKE ?) LIMIT ?,4");
+    mysqli_stmt_bind_param($stmt,"ssssi", $new_str, $new_str, $new_str, $new_str, $n);
+	mysqli_stmt_execute($stmt);
+	$results = mysqli_stmt_get_result($stmt);
+	if (mysqli_num_rows($results) != 0) {
+        $arr = array($str => "<span>".$str."</span>");
+        while($row = mysqli_fetch_assoc($results)){
+            $conta = $conta + 1;
+            $idBlog = $row['idblog'];
+            $titolo_blog = str_replace($str, $arr[$str], $row['Titolo']);
+            $desc_blog = str_replace($str, $arr[$str], $row['Descrizione']);
+            $cat_blog = str_replace($str, $arr[$str], $row['Nome']);
+            $autore_blog = str_replace($str, $arr[$str], $row['Username']);
+            $res.="<tr id='risultati' data-blog-id='{$idBlog}'>
+                <td class='titolo_blog'>".$titolo_blog."</td>
+                <td class='descrizione_blog'>".$desc_blog."</td>
+                <td class='categoria_blog'>".$cat_blog."</td>
+                <td class='autore_blog'>".$autore_blog."</td>
+            </tr>";
+        }
+    }
+    $data = array("data"=>$res, "conta"=>$conta);
+    echo json_encode($data);
+}else if($selezione == "nuovi_risultati_post" and $str!=""){
+    $n = $_GET['numero'];
+    $conta = 0;
+    $res = "";
+    $new_str="%".$str."%";
+    $stmt= mysqli_prepare($link, "SELECT DISTINCT Titolo, Testo, Username, IdBlog FROM post, utente WHERE post.IdUtente=utente.IdUtente  AND (Titolo LIKE ? OR Testo LIKE ? OR Username LIKE ?)LIMIT?,4");
+    mysqli_stmt_bind_param($stmt,"sssi", $new_str, $new_str, $new_str,$n);
+	mysqli_stmt_execute($stmt);
+	$results = mysqli_stmt_get_result($stmt);
+	if (mysqli_num_rows($results) != 0) {
+        $arr = array($str => "<span>".$str."</span>");
+        while($row = mysqli_fetch_assoc($results)){
+            $conta = $conta + 1;
+            $idBlog = $row['IdBlog'];
+            $titolo_post = str_replace($str, $arr[$str], $row['Titolo']);
+            $testo_post = str_replace($str, $arr[$str], $row['Testo']);
+            $autore_post = str_replace($str, $arr[$str], $row['Username']);
+            $res.="<tr id='risultati' data-blog-id='{$idBlog}'>
+                <td class='titolo_post'>".$titolo_post."</td>
+                <td class='testo_post'>".$testo_post."</td>
+                <td class='autore_post'>".$autore_post."</td>
+            </tr>";
+        }
+    }
+    $data = array("data"=>$res, "conta"=>$conta);
+    echo json_encode($data);
 }
 ?>
