73aed2d2c8d091cf0c05a80d5c7faa23e3c98c68
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2024-03-22 15:56:41 +0100

    Update tutti_i_blog.php

--- prova/tutti_i_blog.php
+++ prova/tutti_i_blog.php
@@ -1,5 +1,25 @@
 <?php
 session_start();
+include_once("connect.php");
+
+if (!isset($_SESSION["session_utente"]) || empty($_SESSION["session_utente"])) {
+  session_unset();
+  session_destroy();
+  header("Location: registrazione.php");
+  exit;
+}
+
+$id_utente = $_SESSION["session_utente"];
+$stmt_premium = mysqli_prepare($link, "SELECT Premium FROM utente WHERE IdUtente=?");
+mysqli_stmt_bind_param($stmt_premium, "i", $id_utente);
+mysqli_stmt_execute($stmt_premium);
+$results_premium = mysqli_stmt_get_result($stmt_premium);
+if(mysqli_fetch_assoc($results_premium)["Premium"]==0){
+  $button = "<a href='premium.php'><input type='button' value='Premium'></a>";
+}else{
+    $button = "<a href='insight.php'><input type='button' value='Insight'></a>";
+}
+mysqli_stmt_close($stmt_premium);
 ?>
 <html lang="it" dir="ltr">
   <head>
@@ -10,85 +30,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
-    <script>
-      $(document).ready(function() {
-        $(".griglia_blog").on("click", ".blog", function(){
-          var idBlog = $(this).data("blog-id");
-
-          location.replace("singolo_blog.php?id=" + idBlog);
-        });
-        var numeroBlogCaricati;
-        var tipoSelezionato = "Recenti";
-        var isMessageAppended = false;
-        
-        function caricaBlog(tipo) {
-          $.ajax({
-            url: "tutti_i_blog_select.php",
-            method: "GET",
-            data: { numero: numeroBlogCaricati, tipo: tipo },
-            success: function(data) {
-              if (data.trim() === "Nessun Blog") {
-                $("#caricablog").hide();
-                if (!isMessageAppended) {
-                  $(".griglia_blog").append("<p class='messaggio_fine'><img class='ops' src='foto/ops.png'><br>Ops, sei arrivato in fondo!</p>");
-                  isMessageAppended = true;
-                }
-              } else {
-                $(".messaggio_fine").remove(); 
-                $(".griglia_blog").append(data);
-                numeroBlogCaricati += 9;
-                isMessageAppended = false;
-                $("#caricablog").show(); 
-
-              }
-            },
-            error: function(xhr, status, error) {
-              console.error(error);
-            }
-          });
-        }
-        function resetAndLoadBlog(tipo) {
-          $(".griglia_blog").empty();
-          numeroBlogCaricati = 0;
-          messaggioPresente = false; 
-          caricaBlog(tipo);
-        }
-        $("#blog_recenti").on("click", function() {
-          $(".blog_rec").show();
-          $(".blog_pop").hide();
-          $(".blog_per_u").hide();
-          $("label#blog_recenti").addClass("selected");
-          $("label#blog_popolari").removeClass("selected");
-          $("label#blog_per_utente").removeClass("selected");
-          tipoSelezionato = "Recenti";
-          resetAndLoadBlog(tipoSelezionato);
-        });
-        $("#blog_popolari").on("click", function() {
-          $(".blog_rec").hide();
-          $(".blog_pop").show();
-          $(".blog_per_u").hide();
-          $("label#blog_popolari").addClass("selected");
-          $("label#blog_recenti").removeClass("selected");
-          $("label#blog_per_utente").removeClass("selected");
-          tipoSelezionato = "Popolari";
-          resetAndLoadBlog(tipoSelezionato);
-        });
-        $("#blog_per_utente").on("click", function() {
-          $(".blog_rec").hide();
-          $(".blog_pop").hide();
-          $(".blog_per_u").show();
-          $("label#blog_per_utente").addClass("selected");
-          $("label#blog_recenti").removeClass("selected");
-          $("label#blog_popolari").removeClass("selected");
-          tipoSelezionato = "Autore";
-          resetAndLoadBlog(tipoSelezionato);
-        });
-        $("#caricablog").on("click", function() {
-          caricaBlog(tipoSelezionato);
-        });
-        resetAndLoadBlog(tipoSelezionato);
-      });
-    </script>
+    <script src = "js/tutti_i_blog.js"></script>
   </head>
 <body id="body_tutti_i_blog">
   <header id="header_tutti_i_blog">
@@ -102,7 +44,7 @@ session_start();
         <li><a href="info.php">Info</a></li>
       </ul>
       <div class="buttons">
-        <input type="button" value="Premium">
+        <?php echo $button ?>
         <a href="logout.php"><input type="button" value="Logout"></a>
       </div>
     </nav>
