a267f14790ca2393e675e00b8fc89903b1f9d9fb
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2024-03-22 15:46:25 +0100

    Update i_tuoi_blog.php

--- prova/i_tuoi_blog.php
+++ prova/i_tuoi_blog.php
@@ -1,5 +1,25 @@
 <?php
 session_start();
+include_once("connect.php");
+
+if (!isset($_SESSION["session_utente"]) || empty($_SESSION["session_utente"])) {
+  session_unset();
+  session_destroy();
+  header("Location: registrazione.php");
+  exit;
+}
+
+$id_utente = $_SESSION["session_utente"];
+$stmt_premium = mysqli_prepare($link, "SELECT Premium FROM utente WHERE IdUtente=?");
+mysqli_stmt_bind_param($stmt_premium, "i", $id_utente);
+mysqli_stmt_execute($stmt_premium);
+$results_premium = mysqli_stmt_get_result($stmt_premium);
+if(mysqli_fetch_assoc($results_premium)["Premium"]==0){
+  $button = "<a href='premium.php'><input type='button' value='Premium'></a>";
+}else{
+    $button = "<a href='insight.php'><input type='button' value='Insight'></a>";
+}
+mysqli_stmt_close($stmt_premium);
 ?>
 <html lang="it" dir="ltr">
   <head>
@@ -10,126 +30,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
-    <script>
-      $(document).ready(function() {
-        $.get("blog.php", function(data) {
-          if(data=="Sessione annullata"){
-            location.replace("registrazione.php");
-          }
-          $(".griglia_blog").html(data);
-        });
-        $(".griglia_blog").on("click", ".blog", function(){
-          var idBlog = $(this).data("blog-id");
-
-          location.replace("singolo_blog.php?id=" + idBlog);
-          
-        });
-        $("#crea_blog").click(function(){
-          $('#form_crea_blog').toggle();
-          if($(this).val()=="Annulla"){
-            $(this).val("Crea un nuovo Blog");
-            $(".tuoi_blog").prepend($(this));
-            $(this).css("margin-left","");
-            $("input[value='Crea']").css({"margin-top":""});
-            $(".tuoi_blog .presentazione").css({"margin-top":""});
-          }else{
-            $(this).val("Annulla");
-            $(this).css({"margin-left":"25%"});
-            $("#form_crea_blog").prepend($(this));
-            $("input[value='Crea']").css({"margin-top":"2%"});
-            $(".tuoi_blog .presentazione").css({"margin-top":"5%"});
-          }
-        });
-        $.get("categorie_select.php", function(data) {
-          if(data=="Sessione annullata"){
-            location.replace("registrazione.php");
-          }
-          $("#categoria_blog").html(data);
-        });
-        $("#form_crea_blog").validate({
-          rules : {
-            titolo_blog: {
-              required : true,
-              maxlength:15
-              },
-            descrizione_blog : {
-              required : true,
-              maxlength: 40
-                },
-            categoria_blog : {
-              required : true
-              } 
-            },
-          messages : {
-            titolo_blog: {
-              required: "Inserire un titolo",
-              maxlength: "Inserire massimo 15 caratteri"
-            },
-            descrizione_blog : {
-              required : "Inserire una descrizione",
-              maxlength: "Inserire massimo 40 caratteri"
-            },
-            categoria_blog: {
-              required: "Inserire una categoria"
-            }
-          }
-        });
-        var searchInput = $("#coautori");
-        var searchResults = $("#searchResults");
-        searchInput.on("input", function() {
-          var query = $(this).val();
-          $.ajax({
-            type: "GET",
-            url: "search_coautori.php",
-            data: { query: query },
-            dataType: "html",
-            success: function(data) {
-              searchResults.html(data);
-            }
-          });
-        });
-        searchResults.on("click", "li", function() {
-          if($(this)!="Nessun risultato"){
-            var selezione = $(this).text();
-            var valore = searchInput.val();
-            if (valore) {
-              var elenco = valore.split(' ');
-              if (elenco.indexOf(selezione) === -1) {
-                searchInput.val(valore + selezione);
-              }
-            } else {
-              searchInput.val(selezione);
-            }
-          }
-        });
-        $("#form_crea_blog").on("submit", function(event){
-            if($(this).valid()){
-            $("#error_message").hide();
-            event.preventDefault();
-            var formData = new FormData(this);
-            $.ajax({
-              type: "POST",
-              url: $("#form_crea_blog").attr("action"),
-              processData: false,
-              contentType: false,
-              data: formData,
-              success: function(data){
-                if(data == "OK"){
-                   location.reload();
-                } else{
-                  $("#error_message").show();
-                  if($(data).text().indexOf("cannot be null") !== -1){
-                    $("#error_message").text("Hai raggiunto il numero massimo di blog per un utente standard!")
-                  }else{
-                    $("#error_message").text(data);
-                  }
-                }         
-              }
-            });
-          }  
-        });        
-       }); 
-    </script>
+    <script src = "js/i_tuoi_blog.js"></script>
    </head>
 <body id="body_tuoi_blog">
   <header id="header_tuoi_blog">
@@ -143,7 +44,7 @@ session_start();
         <li><a href="info.php">Info</a></li>
       </ul>
       <div class="buttons">
-        <input type="button" value="Premium">
+        <?php echo $button ?>
         <a href="logout.php"><input type="button" value="Logout"></a>
       </div>
     </nav>
