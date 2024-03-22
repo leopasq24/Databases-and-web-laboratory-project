226330fe3e90496ac447c33397a1cac1b1077ba3
leopasq24 <139567008+leopasq24@users.noreply.github.com>
2024-03-22 15:48:10 +0100

    Update loggato.php

--- prova/loggato.php
+++ prova/loggato.php
@@ -35,19 +35,6 @@ if($i==0){
 	}
 	else{
 		$_SESSION["session_utente"]=$result_array[0];
-	}
-}
-
-if($i==0){
-	$stmt_primo_accesso=mysqli_prepare($link, "SELECT IdUtente FROM blog WHERE IdUtente=? UNION SELECT IdUtente FROM post WHERE IdUtente=? UNION SELECT IdUtente FROM feedback WHERE idUtente=? UNION SELECT IdUtente FROM commenta WHERE idUtente=?");
-	mysqli_stmt_bind_param($stmt_primo_accesso,"iiii",$_SESSION["session_utente"],$_SESSION["session_utente"],$_SESSION["session_utente"],$_SESSION["session_utente"]);
-	mysqli_stmt_execute($stmt_primo_accesso);
-	$result_primo_accesso = mysqli_stmt_get_result($stmt_primo_accesso);
-	$result_array = mysqli_fetch_row($result_primo_accesso);
-	if(empty($result_array)){
-		echo("Primo accesso");
-	}
-	else{
 		echo("OK");
 	}
 }
